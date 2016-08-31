e056f66c2104fe1714876b8e321bedeeeb9472b9
aadams22 <ashleigh.adams22@gmail.com>
2016-08-31 12:01:39 -0500

    init commit

---
+++ server.js
@@ -0,0 +1,36 @@
+var express   = require('express');
+var app       = express();
+// var url       = require('url');
+// var request   = require('request');
+
+
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('port', (process.env.PORT || 8080));
+
+
+app.get('/', function(req, res){
+  res.send('Huzzah! I still work!');
+});
+
+//Units For Testing:
+
+
+app.post('/post', function(req, res){
+
+  var body = {
+    response_type: "in_channel",
+    text: "hi there world"
+  };
+
+  res.send(body);
+
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});

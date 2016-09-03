2ba7f49864e920aea932021914fdb2e75b42bfd4
aadams22 <ashleigh.adams22@gmail.com>
2016-09-02 23:00:26 -0500

    debugging

--- server.js
+++ server.js
@@ -26,75 +26,76 @@ var currentYear   = today.getFullYear();
 
 
 
-var validations = {
-	incompleteParams: function(fail) {
-		return msg = 'There was an error. Please input correct ' + fail + '.'
-	},
-	isDateValid: function(d) {
-		if(d < today) { return validations.incompleteParams(d) }
-	}
-};
+// var validations = {
+// 	incompleteParams: function(fail) {
+// 		return msg = 'There was an error. Please input correct ' + fail + '.'
+// 	},
+// 	isDateValid: function(d) {
+// 		if(d < today) { return validations.incompleteParams(d) }
+// 	}
+// };
 
 
-var conversion = {
-	convertDate: function(date) {
-		//checks if the date is in place 1 or 2 of the date
-		if (!isNaN(date.split(/[ ]+/)[0])) {  
-			return months.indexOf(date.split(/[ ]+/)[0] + 1)
-		}else if(!isNaN(date.split(/[ ]+/)[1])) {
-			return months.indexOf(date.split(/[ ]+/)[1] + 1)
-		}
+// var conversion = {
+// 	convertDate: function(date) {
+// 		//checks if the date is in place 1 or 2 of the date
+// 		if (!isNaN(date.split(/[ ]+/)[0])) {  
+// 			return months.indexOf(date.split(/[ ]+/)[0] + 1)
+// 		}else if(!isNaN(date.split(/[ ]+/)[1])) {
+// 			return months.indexOf(date.split(/[ ]+/)[1] + 1)
+// 		}
 
-	},
-	convertCity: function(city) {
-		//runs a backbone query on the airport-codes module to find the IATA for user's input city
-			return airports.findWhere({ 'city' : city }).get('iata'); 
-	},
-	addYear: function(date){
-		console.log('this is split date: ', date.split(''));
-		if(!date.split('').length == 5) { 
-			return currentYear + '-' + date 
-		};
-	}
-};
+// 	},
+// 	convertCity: function(city) {
+// 		//runs a backbone query on the airport-codes module to find the IATA for user's input city
+// 			return airports.findWhere({ 'city' : city }).get('iata'); 
+// 	},
+// 	addYear: function(date){
+// 		console.log('this is split date: ', date.split(''));
+// 		if(!date.split('').length == 5) { 
+// 			return currentYear + '-' + date 
+// 		};
+// 	}
+// };
 
 
 //===================================================================================
-// function findDepartures(data) {
-// 	for (var i = 0; i < data.legs.length; i++) {
-// 		flights.push({
-// 									"flight number": data.legs[i].segments[0].flightNumber,
-// 									"departure": data.legs[i].segments[0].departureTime,
-// 									"arrival": data.legs[i].segments[0].arrivalTime,
-// 									"airline": data.legs[i].segments[0].airlineName,
-// 									"stops": data.legs[i].segments[0].stops
-// 								});
+function findDepartures(data) {
+	for (var i = 0; i < data.legs.length; i++) {
+		flights.push({
+									"flight number": data.legs[i].segments[0].flightNumber,
+									"departure": data.legs[i].segments[0].departureTime,
+									"arrival": data.legs[i].segments[0].arrivalTime,
+									"airline": data.legs[i].segments[0].airlineName,
+									"stops": data.legs[i].segments[0].stops
+								});
 
-// 	};
-// 	return flights;
-// }
+	};
+	return flights;
+}
 
 
-// function getflightData(origin, destination, departureDate, airline) {
+function getflightData() {
+	var url     = "http://terminal2.expedia.com/x/mflights/search?departureAirport=LAX&arrivalAirport=ORD&departureDate=2016-10-22&apikey=" + process.env.FLIGHTBOT_EXPEDIA_API_KEY;
 
-// 	var url     = "http://terminal2.expedia.com/x/mflights/search?departureAirport=" + origin + "&arrivalAirport=" + destination + "&departureDate=" + departureDate + "&airlineName=" + airline + "&apikey=" + process.env.FLIGHTBOT_EXPEDIA_API_KEY;
-// 	var method  = 'GET';
-// 	var async   = true;
+	// var url     = "http://terminal2.expedia.com/x/mflights/search?departureAirport=" + origin + "&arrivalAirport=" + destination + "&departureDate=" + departureDate + "&airlineName=" + airline + "&apikey=" + process.env.FLIGHTBOT_EXPEDIA_API_KEY;
+	var method  = 'GET';
+	var async   = true;
 	var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 	var request = new XMLHttpRequest();
 
-// 	request.onload = function() {
-// 		var status = request.status;
-// 		var data = JSON.parse(request.responseText);
-// 		console.log("status ", status);
-// 		console.log("data", data);
-// 		findDepartures(data);
-// 	};
+	request.onload = function() {
+		var status = request.status;
+		var data = JSON.parse(request.responseText);
+		console.log("status ", status);
+		console.log("data", data);
+		findDepartures(data);
+	};
 
-// 	request.open(method, url, async);
-// 	request.setRequestHeader("Content-Type", "json;");
-// 	request.send();
-// }
+	request.open(method, url, async);
+	request.setRequestHeader("Content-Type", "json;");
+	request.send();
+}
 //===================================================================================
 
 
@@ -108,10 +109,10 @@ app.get('/', function(req, res){
 //need to find a solution for cities with 2 words that include a white space
 app.post('/post', function(req, res){
 	// userParams    = req.body.text.split(/[ ]+/);
-	origin 				= req.body.text.split(/[ ]+/)[0];
-	destination 	= req.body.text.split(/[ ]+/)[1];
-	departureDate = req.body.text.split(/[ ]+/)[2];
-	airline				= req.body.text.split(/[ ]+/)[3];
+	// origin 				= req.body.text.split(/[ ]+/)[0];
+	// destination 	= req.body.text.split(/[ ]+/)[1];
+	// departureDate = req.body.text.split(/[ ]+/)[2];
+	// airline				= req.body.text.split(/[ ]+/)[3];
 	
 
 
@@ -123,12 +124,14 @@ app.post('/post', function(req, res){
 	// else if(airline) 			{ validations.incompleteParams(airline) }
 	// else									  { getflightData(origin, destination, departureDate, airline) }
 
-	var msg = ' origin: ' + origin 
-				+ ' destination: ' + destination 
-				+ ' departure date: ' + departureDate 
-				+ ' arrival date: ' + returnDate;
+	// msg = ' origin: ' + origin 
+	// 			+ ' destination: ' + destination 
+	// 			+ ' departure date: ' + departureDate 
+	// 			+ ' arrival date: ' + returnDate;
 
 	// msg = "This was your request:" + r + "These are your options:" + flights;
+
+	msg = getflightData();
 
   var body = {
     response_type: "in_channel",

efeae5d04209f976355633df98ac3adc576b1ff4
aadams22 <ashleigh.adams22@gmail.com>
2016-09-02 21:53:07 -0500

    adding expedia api access and validations of user input

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express   = require('express');
 var app       = express();
-var request   = require('request');
 var airports  = require('airport-codes');
 
 
@@ -12,14 +11,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', (process.env.PORT || 8080));
 
 
-app.get('/', function(req, res){
-  res.send('Huzzah! I still work!');
-});
+// /getflights [origin, destination, departureDate, returnDate, airline]
+// /getflights [origin, destination, departureDate, airline]
+
+var msg 			  	= null;
+var userParams    = null;
+var origin			  = null;
+var destination   = null;
+var departureDate = null;
+var returnDate 		= null;
+var airline				= null;
+var today 				= new Date();
+var currentYear   = today.getFullYear();
+
+// function FlightSearch() {
+// 	this.origin = "";
+// 	this.destination = "";
+// 	this.departureDate = "";
+// 	this.returnDate = "";
+// 	this.airline = "";
+// };
 
 
-//2. find solution for formatting dates = year-mo-da
-
-var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+var validations = {
+	incompleteParams: function(fail) {
+		return msg = 'There was an error. Please input correct ' + fail + '.'
+	},
+	isDateValid: function(d) {
+		if(d < today) { return validations.incompleteParams(d) }
+	}
+};
 
 
 var conversion = {
@@ -34,44 +55,44 @@ var conversion = {
 	},
 	convertCity: function(city) {
 		//runs a backbone query on the airport-codes module to find the IATA for user's input city
-		return airports.findWhere({ 'city' : city }).get('iata'); 		 
+		if(airports.findWhere({ 'city' : city }).get('iata')) { 
+			return airports.findWhere({ 'city' : city }).get('iata'); 
+		}else{ 
+			return validations.incompleteParams(city);
+		};
+	},
+	addYear: function(date){
+		console.log('this is split date: ', date.split(''));
+		if(!date.split('').length == 5) { 
+			return currentYear + '-' + date 
+		};
 	}
-}
+};
 
+//Sends to deployed view to make sure it's up and running! 
+app.get('/', function(req, res){
+  res.send("Huzzah! I still work! Now let's have some tea.");
+});
 
 
 app.post('/post', function(req, res){
-	var origin 				= conversion.convertCity(req.body.text.split(/[ ]+/)[0]);
-	var destination 	= conversion.convertCity(req.body.text.split(/[ ]+/)[1]);
-	var departureDate = req.body.text.split(/[ ]+/)[2];
-	var returnDate 		= req.body.text.split(/[ ]+/)[3];
+	userParams    = req.body.text.split(/[ ]+/);
+	origin 				= conversion.convertCity(userParams[0].trim());
+	destination 	= conversion.convertCity(userParams[1].trim());
+	departureDate = userParams[2].trim();
+	returnDate 		= userParams[3].trim();
+	airline				= userParams[4].trim();
 	
-	//!!Future implimentation: allowences for written months
-	// if ( departureDate.includes(/^[A-Za-z ]+$/) ) { conversion.convertDate(departureDate) };
-	// if ( returnDate.includes(/^[A-Za-z ]+$/) ) { conversion.convertDate(returnDate) };
+
+
+	//if the departure param exists, check and see if the date is in the past, if the departure date does not exist, send error message
+	if(departureDate) { validations.isDateValid(departureDate); }else { validations.incompleteParams(departureDate) }
+	//if return param exists, check and see if the date is in the past. This param is not required so other validations do not need to take place
+	if(returnDate) { validations.isDateValid(returnDate); }
 
 
 
-	// request({
-	// 	url: 'http://terminal2.expedia.com/x/mflights/search?departureAirport=' + origin + '&arrivalAirport=' + destination + '&departureDate=' + departureDate + '&returnDate=' + returnDate + '&apikey=' + process.env.FLIGHTBOT_EXPEDIA_API_KEY,
-	// 	method: 'POST',
-	// 	headers: {
-	// 		'Content-Type': 'JSON',
-	// 	}, function(err, res, data) {
-
-	// 		if(err) { 
-	// 			var msg = err;
-	// 		}else { 
-	// 			console.log(res.statusCode, body); 
-	// 			var msg = 'this is from expedia: ' + res.statusCode + ' ' + data;
-	// 		}
-
-	// 	}
-	// });
-
-	var msg = 'origin: ' + origin + '. destination: ' + destination + ' departure date: ' + departureDate + ' arrival date: ' + returnDate;
-
-
+	msg = 'origin: ' + origin + '. destination: ' + destination + ' departure date: ' + departureDate + ' arrival date: ' + returnDate;
 
   var body = {
     response_type: "in_channel",
@@ -81,10 +102,6 @@ app.post('/post', function(req, res){
   res.send(body);
 
 });
-
-
-
-
 
 
 app.listen(app.get('port'), function() {
